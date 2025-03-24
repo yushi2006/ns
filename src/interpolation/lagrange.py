d43d3854b284513225f036df72202074a5719634
Yusuf Shihata <yusufshihata2006@gmail.com>
2025-03-24 11:23:22 +0200

    some changes

--- src/interpolation/lagrange.py
+++ src/interpolation/lagrange.py
@@ -9,13 +9,11 @@ class Lagrange(Interpolation):
         y_interpolated = 0
 
         for k in range(n):
-            # Compute L_k(x)
             L_k = 1
             for i in range(n):
                 if i != k:
                     L_k *= (new_x - self.x[i]) / (self.x[k] - self.x[i])
 
-            # Multiply L_k(x) by y_k and sum
             y_interpolated += self.y[k] * L_k
 
         return y_interpolated
@@ -25,13 +23,10 @@ class Lagrange(Interpolation):
         x_smooth = np.linspace(min(self.x), max(self.x), 1000)
         y_smooth = [self.interpolate(x) for x in x_smooth]
 
-        # Plot original points
         plt.scatter(self.x, self.y, color='red', marker='o', edgecolors='black', label="Original Points")
 
-        # Plot interpolated curve
         plt.plot(x_smooth, y_smooth, color='blue', linestyle='-', linewidth=2, label="Lagrange Interpolation")
 
-        # Labels and grid
         plt.xlabel("X-axis")
         plt.ylabel("Y-axis")
         plt.title("Lagrange Polynomial Interpolation")
